5dd595e8a369e1cb60f94c4b915d8676e78c5f26
vikrant <vikrant.immeasurable@gmail.com>
2019-03-15 13:37:42 +0530

    body parser added

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+app.all('/dishes', (req,res,next) => {
+
+    res.statusCode =200;
+    res.setHeader('Content-Type', 'text/palin');
+    next();
+});
+
+app.get('/dishes', (req,res,next) => {
+    res.end('Wil send all the dishes to you!');
+});
+
+app.post('dishes', (req,res,next) => {
+    
+    res.end('Will add the dish: ' + req.body.name +
+     'with details' + req.body.description );
+
+}); 
+
 app.use(express.static(__dirname+ '/public'));
 
 app.use((req, res, next ) => {   
